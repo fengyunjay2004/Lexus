7e1ec979699cb33dc7a17a9a5baf21a54a948c08
fengyunjay2004 <34676898+fengyunjay2004@users.noreply.github.com>
2024-10-02 14:33:05 +0800

    Update sss.js

--- sss.js
+++ sss.js
@@ -4,6 +4,9 @@
 ===================
 [rewrite_local]
 ^https:\/\/fwdt\.shengongshe\.org\/sgsWchartApi\/api\/MyIntegral\/checkIsSign url script-request-header https://raw.githubusercontent.com/fengyunjay2004/Lexus/refs/heads/main/sss.js?token=GHSAT0AAAAAACYJK2HBOIHPJNGW4GM45ATQZX4476A
+[MITM]
+hostname = fwdt.shengongshe.org
+
 
  */
 const APIKey = "yy_10000";
