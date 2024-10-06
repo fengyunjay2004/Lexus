dd240a24a569b8731429da613dc4e2640b1b2735
fengyunjay2004 <34676898+fengyunjay2004@users.noreply.github.com>
2024-10-06 17:02:09 +0800

    Update fzyyj-sign.js

--- fzyyj-sign.js
+++ fzyyj-sign.js
@@ -19,7 +19,7 @@ if ($request) {
         let cookie = $request.headers['Cookie'];
         if (cookie) {
             $prefs.setValueForKey(cookie, "cookieKey");  // 保存 Cookie 到本地存储
-            $notify("成功", "Cookie 已保存", `以下是获取到的 Cookie:\n${cookie}`);
+            //$notify("成功", "Cookie 已保存", `以下是获取到的 Cookie:\n${cookie}`);
             let tokenMatch = cookie.match(/token=([^;]+)/);
             if (tokenMatch) {
                 // 提取到的 token 值
@@ -29,7 +29,7 @@ if ($request) {
                 $prefs.setValueForKey(token, "AuthorizationKey");  
                 
                 // 发送通知，显示提取的 token
-                $notify("成功", "Authorization 已提取", `Authorization:\n${token}`);
+                $notify("成功", "Authorization 已提取", `以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${token}`);
                 console.log(`Authorization: ${token}`);
             } else {
                 // 未找到 token 时，发送通知
