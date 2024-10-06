1f5aa1c0e616165a29597d9df43bb6ef1d0a34fd
fengyunjay2004 <34676898+fengyunjay2004@users.noreply.github.com>
2024-10-06 17:42:19 +0800

    Update fzyyj-sign.js

--- fzyyj-sign.js
+++ fzyyj-sign.js
@@ -20,8 +20,8 @@ if ($request) {
         let cookie = $request.headers['Cookie'];
         if (cookie) {
             $prefs.setValueForKey(cookie, "cookieKey");  // 保存 Cookie 到本地存储
+            //$notify("成功", "Cookie 已保存", `以下是获取到的 Cookie:\n${cookie}`);
             console.log(`cookie值:\n${cookie}`);
-            
             let tokenMatch = cookie.match(/token=([^;]+)/);
             if (tokenMatch) {
                 // 提取到的 token 值
@@ -30,19 +30,15 @@ if ($request) {
                 // 保存 token 到本地存储
                 $prefs.setValueForKey(token, "AuthorizationKey");  
                 
-                // 发送通知，显示提取的 token 和 cookie
-                $notify("成功", "Authorization 已提取", `以下是获取到的 Cookie 值:\n${cookie}\n\n以下是获取到的 Authorization 值:\n${token}`);
+                // 发送通知，显示提取的 token
+                $notify("成功", "Authorization 已提取", `以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${token}`);
                 console.log(`Authorization值:\n${token}`);
-                
-                // 调用发送钉钉消息的函数
-                sendDingTalkMessage(`成功提取的 Cookie: ${cookie}\n提取的 Authorization: ${token}`);
             } else {
                 // 未找到 token 时，发送通知
                 $notify("失败", "未找到 token", "请检查 Cookie");
-                sendDingTalkMessage("未找到 token，请检查 Cookie。");
             }
         }
-
+        
         let authorization = $request.headers['Authorization'];
         if (authorization) {
             $prefs.setValueForKey(authorization, "authKey");  // 保存 Authorization
@@ -51,39 +47,4 @@ if ($request) {
     }
 }
 
-// 发送钉钉消息的函数
-function sendDingTalkMessage(content) {
-    let webhookURL = "90315bbbc30de5de178af71a7cf78f9b78d8983957ce20a2999166e0f0a61526";  // 将此处替换为你的钉钉机器人 Webhook 地址
-    let body = {
-        "msgtype": "text",
-        "text": {
-            "content": content
-        }
-    };
-
-    let options = {
-        url: webhookURL,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    };
-
-    // 发送 POST 请求到钉钉
-    $httpClient.post(options, function(error, response, data) {
-        if (error) {
-            console.log("钉钉消息发送失败: " + error);
-            $notify("钉钉通知", "发送失败", error);
-        } else {
-            console.log("钉钉消息发送成功: " + data);
-            $notify("钉钉通知", "发送成功", "钉钉消息发送成功");
-        }
-    });
-}
-
-$done();  // 结束脚本
-
-
-
-
-
+$done();
